23d7a288d21a093f5feddddca7d6024c8a1f3ac5
Wesley Chang <weschang15@gmail.com>
2019-07-30 23:09:26 -0700

    Added colors

--- src/config/theme.js
+++ src/config/theme.js
@@ -3,5 +3,7 @@ export default {
   purple: "rgba(59, 53, 97, 1)",
   yellow: "rgba(234, 217, 76, 1)",
   gray: "rgba(209, 209, 209, 1)",
-  teal: "rgba(81, 163, 163, 1)"
+  teal: "rgba(81, 163, 163, 1)",
+  black: "rgba(32, 28, 41, 1)",
+  white: "rgba(242, 239, 223, 1)"
 };
