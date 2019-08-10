a12ed3015ebb48ab8aa42b349a21b98ea7fd4efa
Wesley Chang <weschang15@gmail.com>
2019-08-09 22:09:03 -0700

    Created grid layout

--- src/Components/routes/AdminRoutes.js
+++ src/Components/routes/AdminRoutes.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+
 import FloatingNavBar from "../../Components/Navbar/FloatingNavBar";
-import { Sites } from "../../Pages";
+import { Sites, Dashboard } from "../../Pages";
+import TopNavBar from "../Navbar/TopNavBar";
+
+const Container = styled.div`
+  background-color: transparent;
+  position: relative;
+  /* Ensures subroutes are properly spaced inwords from floating nav */
+  /* padding: 1em 1em 1em calc(66px + 1em); */
+  display: grid;
+  grid-template-columns: minmax(0, 66px) auto;
+  grid-template-rows: auto;
+  grid-gap: 1rem;
+  grid-template-areas:
+    ". header"
+    "sidebar content"
+    "sidebar content"
+    "sidebar content"
+    "sidebar content";
+  padding: 1em;
+`;
 
 function AdminRoutes({ match: { path } }) {
   return (
-    <div>
+    <Container>
+      <TopNavBar />
       <FloatingNavBar />
-      <Route path={`${path}/sites`} component={Sites} exact />
-    </div>
+      <Route path={`${path}`} component={Dashboard} exact />
+      <Route path={`${path}/sites`} component={Sites} />
+    </Container>
   );
 }
 
-export default withRouter(AdminRoutes);
+export default AdminRoutes;
