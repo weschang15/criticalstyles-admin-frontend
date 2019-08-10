50884d3aafdbebe11877db75e25e9e249a39bfe1
Wesley Chang <weschang15@gmail.com>
2019-08-09 22:09:32 -0700

    Added new icons

--- src/Elements/Icons.js
+++ src/Elements/Icons.js
@@ -108,8 +108,19 @@ function Icons(props = {}) {
         </SVG>
       );
 
+    case "window-new":
+      return (
+        <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...props}>
+          <path d="M9 10V8h2v2h2v2h-2v2H9v-2H7v-2h2zM0 3c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm2 2v12h16V5H2z" />
+        </SVG>
+      );
+
     default:
-      return;
+      return (
+        <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...props}>
+          <path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z" />
+        </SVG>
+      );
   }
 }
 
