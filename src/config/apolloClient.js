e70d9486f1b6e6ed6f875c20f6bda3c07992ad6b
Wesley Chang <weschang15@gmail.com>
2019-08-15 22:57:57 -0700

    Better errors

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -9,7 +9,11 @@ export const apolloClient = new ApolloClient({
     if (graphQLErrors) {
       graphQLErrors.map(({ message, locations, path }) =>
         console.log(
-          `[GraphQL error]: Message ${message}, Location: ${locations}, Path: ${path}`
+          `[GraphQL error]: Message ${message}, Location: ${JSON.stringify(
+            locations,
+            null,
+            2
+          )}, Path: ${path}`
         )
       );
     }
