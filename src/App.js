77783ba057708010ee6ea3b614fadb910e6f2ea4
Wesley Chang <weschang15@gmail.com>
2019-08-15 22:55:42 -0700

    Updated to use on complete hook and reducer dispatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
-import theme from "./config/theme";
+
 import PublicRoutes from "./Components/routes/PublicRoutes";
+import theme from "./config/theme";
+import { useQuery } from "react-apollo";
+import { AuthDispatch } from "./contexts/AuthContext";
+import { LOGIN_USER } from "./actions";
+import { CURRENT_USER } from "./Queries";
 
 function App() {
+  const dispatch = useContext(AuthDispatch);
+  const { data, loading } = useQuery(CURRENT_USER, {
+    onCompleted: () => {
+      if (data && data.currentUser) {
+        const { user, ok: authenticated } = data.currentUser;
+        const payload = authenticated
+          ? {
+              user,
+              authenticated,
+              accountId: user.accounts[0]._id
+            }
+          : {};
+
+        dispatch({ type: LOGIN_USER, payload });
+      }
+    }
+  });
+
+  if (loading) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
