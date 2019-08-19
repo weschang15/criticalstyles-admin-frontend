2443640dbf2f0440633556e5f6d41c95eb19d9ff
Wesley Chang <weschang15@gmail.com>
2019-08-18 22:17:49 -0700

    Created parent page for querying sites

--- src/Pages/Sites/Sites.js
+++ src/Pages/Sites/Sites.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { useQuery } from "react-apollo";
+import { AuthContext } from "../../contexts/AuthContext";
+import SiteList from "../../Components/Sites/SiteList";
+import { GET_ACCOUNT } from "../../Queries";
 
-function Sites() {
+const Section = styled.section`
+  grid-area: content;
+  grid-row: auto / auto;
+`;
+
+function Sites({ location }) {
+  const { accountId } = useContext(AuthContext);
+  const {
+    data: { getAccount },
+    loading
+  } = useQuery(GET_ACCOUNT, {
+    variables: {
+      input: {
+        id: accountId
+      }
+    }
+  });
+
+  const sites = getAccount ? getAccount.account.sites : [];
   return (
-    <div>
-      <h1>Sites</h1>
-    </div>
+    <>
+      <Section>
+        <SiteList loading={loading} sites={sites} location={location} />
+      </Section>
+    </>
   );
 }
 
