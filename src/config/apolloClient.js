abf2d6adca8388a44083bdc10c255655d6ce2c27
Wesley Chang <weschang15@gmail.com>
2019-08-19 19:58:27 -0700

    Updated apollo client to use advanced config for ws

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,25 +1,57 @@
-import ApolloClient, { InMemoryCache } from "apollo-boost";
-const { REACT_APP_GRAPHQL_SERVER } = process.env;
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { from, split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { onError } from "apollo-link-error";
+import { getMainDefinition } from "apollo-utilities";
+
+const { REACT_APP_GRAPHQL_SERVER, REACT_APP_WEBSOCKET_SERVER } = process.env;
+
+const wsLink = new WebSocketLink({
+  uri: REACT_APP_WEBSOCKET_SERVER,
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: REACT_APP_GRAPHQL_SERVER,
+  credentials: "include"
+});
+
+const transportLinks = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 export const apolloClient = new ApolloClient({
-  uri: REACT_APP_GRAPHQL_SERVER,
-  cache: new InMemoryCache(),
-  credentials: "include",
-  onError: ({ graphQLErrors, networkError }) => {
-    if (graphQLErrors) {
-      graphQLErrors.map(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message ${message}, Location: ${JSON.stringify(
-            locations,
-            null,
-            2
-          )}, Path: ${path}`
-        )
-      );
-    }
+  link: from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message ${message}, Location: ${JSON.stringify(
+              locations,
+              null,
+              2
+            )}, Path: ${path}`
+          )
+        );
+      }
 
-    if (networkError) {
-      console.log(`[Network error]: ${networkError}`);
-    }
-  }
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
+    }),
+    transportLinks
+  ]),
+  cache: new InMemoryCache()
 });
