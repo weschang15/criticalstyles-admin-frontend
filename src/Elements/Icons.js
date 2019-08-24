97db101d76a2c2bb2ac58185b009b06e8f4a449b
Wesley Chang <weschang15@gmail.com>
2019-08-23 22:56:28 -0700

    Added modal image and breadcrumb separator

--- src/Elements/Icons.js
+++ src/Elements/Icons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const SVG = styled.svg`
@@ -115,6 +116,13 @@ function Icons(props = {}) {
         </SVG>
       );
 
+    case "chevron-right":
+      return (
+        <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...props}>
+          <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+        </SVG>
+      );
+
     default:
       return (
         <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...props}>
@@ -123,5 +131,9 @@ function Icons(props = {}) {
       );
   }
 }
+
+Icons.propTypes = {
+  icon: PropTypes.string
+};
 
 export default Icons;
