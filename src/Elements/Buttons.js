5eea1132b060f65b9a17e2be25729e93bc3fbdc6
Wesley Chang <weschang15@gmail.com>
2019-08-24 16:38:24 -0700

    Removed dead code

--- src/Elements/Buttons.js
+++ src/Elements/Buttons.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken, transparentize } from "polished";
+import { darken } from "polished";
 
 export const PrimaryButton = styled.button`
   background-color: ${({ bgColor, theme }) =>
