7940ac0c4eba87129e4906ad10b7c9a674a5c96e
Wesley Chang <weschang15@gmail.com>
2019-08-24 16:38:38 -0700

    Exported Pages archive

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,4 +2,5 @@ export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as Home } from "./Home/Home";
 export { default as Join } from "./Join/Join";
 export { default as Logout } from "./Logout/Logout";
+export { default as Site } from "./Sites/Site";
 export { default as Sites } from "./Sites/Sites";
