11ad9262d79a96e6f0f35bae3f5857d232acf520
Wesley Chang <weschang15@gmail.com>
2019-08-23 22:57:37 -0700

    Updated modal styles and toggled when submitted

--- src/Components/Sites/AddSiteModal.js
+++ src/Components/Sites/AddSiteModal.js
@@ -1,20 +1,66 @@
 import React, { useContext, useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import styled from "styled-components";
-
-import { Fields, Modal, Spinner } from "../../Elements";
-import { CREATE_SITE } from "../../Mutations";
 import { AuthContext } from "../../contexts/AuthContext";
+import { CREATE_SITE } from "../../Mutations";
+import ModalImage from "../../images/create-site.svg";
+
+import {
+  Fields,
+  Modal,
+  Spinner,
+  FieldLabel,
+  PrimaryButton
+} from "../../Elements";
 
 const Form = styled.form`
   display: flex;
-  flex-flow: row wrap;
+  flex-direction: column;
+  justify-content: space-between;
   input[type="text"],
   input[type="url"] {
-    flex: 1;
+    background-color: ${({ theme }) => theme.gray};
+    box-shadow: none;
+    margin: 0 0 1em;
+    width: 100%;
+  }
+`;
+
+const Group = styled.div`
+  padding: 1em;
+  &:last-of-type {
+    display: flex;
+    justify-content: flex-end;
+    background-color: ${({ theme }) => theme.gray};
+    margin: 0;
+    padding: 0.5em 1em;
+    width: 100%;
   }
 
   button {
+    margin-right: 1em;
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+`;
+
+const ModalHeader = styled.header`
+  align-items: flex-end;
+  display: flex;
+  padding: 1em 1em 0;
+  figure {
+    margin: 0 1em 0 0;
+    flex: 1 0 120px;
+    img {
+      display: block;
+    }
+  }
+  h5 {
+    margin-top: 0;
+  }
+  p {
+    font-size: 16px;
     margin: 0;
   }
 `;
@@ -41,8 +87,6 @@ function AddSiteModal({ on, toggle }) {
       }
     });
 
-    console.log(data);
-
     if (data && data.createSite) {
       const {
         createSite: { ok, errors }
@@ -52,41 +96,71 @@ function AddSiteModal({ on, toggle }) {
       }
 
       setFields(INITIAL_FIELDS);
+      toggle();
     }
+  };
+
+  const cancel = e => {
+    e.preventDefault();
+    setFields(INITIAL_FIELDS);
+    toggle();
   };
 
   return (
     <Modal toggle={toggle} on={on}>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <>
+      <ModalHeader>
+        <figure>
+          <img src={ModalImage} alt="Modal for add a new site" />
+        </figure>
+        <div>
+          <h5>Create Site</h5>
+          <p>
+            Your sites allow you to organize all of your generated critical CSS
+            files
+          </p>
+        </div>
+      </ModalHeader>
+      <>
+        {errors.length > 0 && (
           <ul>
             {errors.map(({ message }, i) => (
               <li key={i}>{message}</li>
             ))}
           </ul>
-          <Form onSubmit={handleSubmit}>
+        )}
+        <Form onSubmit={handleSubmit}>
+          <Group>
+            <FieldLabel htmlFor="name">Name</FieldLabel>
             <Fields
               type="text"
+              id="name"
               placeholder="Corporate website"
               onChange={handleChange}
               value={fields.name}
               name="name"
               required
             />
+            <FieldLabel htmlFor="url">URL</FieldLabel>
             <Fields
               type="url"
+              id="url"
               placeholder="https://criticalstyles.com"
               onChange={handleChange}
               value={fields.url}
               name="url"
               required
             />
-            <Fields type="submit">Submit</Fields>
-          </Form>
-        </>
-      )}
+          </Group>
+          <Group>
+            <PrimaryButton onClick={cancel} override="gray">
+              Cancel
+            </PrimaryButton>
+            <Fields type="submit">
+              {loading ? <Spinner /> : "Create Sites"}
+            </Fields>
+          </Group>
+        </Form>
+      </>
     </Modal>
   );
 }
