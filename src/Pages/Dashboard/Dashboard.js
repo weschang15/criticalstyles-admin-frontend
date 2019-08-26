5d1e7b75d848b2b53e7ed996126ffd6d3ea57bb4
Wesley Chang <weschang15@gmail.com>
2019-08-25 18:18:01 -0700

    Added new imagery and service utility toggle to dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import AddUserToggle from "../../Components/Toggles/AddUserToggle";
+import ToggleServiceUtility from "../../Components/Toggles/ToggleServiceUtility";
 
-const Section = styled.section``;
+const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(auto, 420px));
+  grid-gap: 30px;
+`;
 
 function Dashboard() {
   return (
     <>
       <Section>
         <AddUserToggle />
+        <ToggleServiceUtility />
       </Section>
     </>
   );
