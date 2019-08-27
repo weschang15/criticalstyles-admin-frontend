aeda89ea712108627f4b0c9da60e04097fe4c11c
Wesley Chang <weschang15@gmail.com>
2019-08-27 13:57:06 -0700

    Removed undefined import

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,4 @@
 export { default as Dashboard } from "./Dashboard/Dashboard";
-export { default as Join } from "./Join/Join";
 export { default as Logout } from "./Logout/Logout";
 export { default as Site } from "./Sites/Site";
 export { default as Sites } from "./Sites/Sites";
