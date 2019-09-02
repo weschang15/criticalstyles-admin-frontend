6497f52065001830ba1678fb6446a5c264cac9e1
Wesley Chang <weschang15@gmail.com>
2019-09-01 17:08:24 -0700

    Added focus outline

--- src/Components/Breadcrumbs/Breadcrumb.js
+++ src/Components/Breadcrumbs/Breadcrumb.js
@@ -3,28 +3,32 @@ import styled from "styled-components";
 
 const Breadcrumb = styled.li`
   color: ${({ theme }) => darken(0.25, theme.gray)};
+  border-radius: 12px;
   font-size: 14px;
   font-weight: 600;
   line-height: 1;
   margin-right: 0.25em;
 
+  &:focus-within {
+    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.blue};
+  }
+
   a {
+    display: block;
+    padding: 0.5em;
     text-decoration: none;
+
     &:focus,
     &:hover {
       color: ${({ theme }) => theme.blue};
+      outline: none;
     }
   }
 
   &:not(:only-child):last-of-type {
     background-color: ${({ theme }) => transparentize(0.9, theme.blue)};
-    border-radius: 12px;
     color: ${({ theme }) => theme.blue};
     margin-right: 0;
-    a {
-      padding: 0.5em;
-      display: block;
-    }
   }
 `;
 
