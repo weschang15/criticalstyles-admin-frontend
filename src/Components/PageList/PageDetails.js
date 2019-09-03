1e91c4353c79bfce213a067abc84e2ca49d45221
Wesley Chang <weschang15@gmail.com>
2019-09-02 22:05:31 -0700

    Refactored

--- src/Components/PageList/PageDetails.js
+++ src/Components/PageList/PageDetails.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Drawer, LinkButton, Toggle } from "../../Elements";
-import Page from "./Page";
+import Icons from "../../Elements/Icons";
+import MoneyMakerHeader from "../MoneyMaker/MoneyMakerHeader";
+import MoneyMakerResults from "../MoneyMaker/MoneyMakerResults";
 
-function PageDetails(props = {}) {
+function PageDetails({ name, stylesheet, url }) {
   return (
     <Toggle>
       {({ on, toggle }) => {
         return (
           <>
             <Drawer on={on} toggle={toggle}>
-              <Page {...props} />
+              <div>
+                <MoneyMakerHeader>
+                  <h3>{name}</h3>
+                  <p>
+                    <Icons icon="url" width="17px" fill="currentColor" />
+                    {url}
+                  </p>
+                </MoneyMakerHeader>
+                <MoneyMakerResults ccss={stylesheet} />
+              </div>
             </Drawer>
             <LinkButton onClick={toggle}>View</LinkButton>
           </>
