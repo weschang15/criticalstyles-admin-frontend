48a52f62e3adf1b8350b150f4abfe6a90533c612
Wesley Chang <weschang15@gmail.com>
2019-09-02 22:05:39 -0700

    I guess reordered

--- src/Elements/Table/TableHeader.js
+++ src/Elements/Table/TableHeader.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
 import { darken } from "polished";
+import styled from "styled-components";
 
 const TableHeader = styled.thead`
   background-color: ${({ theme }) => theme.gray};
