518f5f4389eb8a146498ba43e9ee398e91d36cbc
Wesley Chang <weschang15@gmail.com>
2019-09-08 14:32:07 -0700

    feat: added snackbar component for notifications

---
+++ src/Components/Snackbar/Snackbar.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import React from "react";
+import styled from "styled-components";
+import { notificationTheme } from "../../config/theme";
+
+const SnackbarWrapper = styled.div`
+  margin: 2em 0;
+  padding: 1em;
+  background-color: ${({ theme, type }) => theme[type].bgColor};
+  border: ${({ theme, type }) => `1px solid  ${theme[type].border}`};
+  color: ${({ theme, type }) => theme[type].color};
+  font-size: 16px;
+  border-radius: 4px;
+`;
+
+function Snackbar({ message, ...rest }) {
+  return (
+    <SnackbarWrapper theme={notificationTheme} {...rest}>
+      {message}
+    </SnackbarWrapper>
+  );
+}
+
+Snackbar.defaultProps = {
+  type: "success"
+};
+
+Snackbar.propTypes = {
+  type: PropTypes.oneOf(["error", "success"]).isRequired,
+  message: PropTypes.string.isRequired
+};
+
+export default Snackbar;
