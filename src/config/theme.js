ae96f4f4315aaba2c9e9b94e50d305833580fdc3
Wesley Chang <weschang15@gmail.com>
2019-09-08 14:32:41 -0700

    feat: added theme for notifications

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,3 +1,16 @@
+export const notificationTheme = {
+  error: {
+    bgColor: "rgba(221, 115, 115, 0.1)",
+    border: "rgba(221, 115, 115, 1)",
+    color: "rgba(221, 115, 115, 1)"
+  },
+  success: {
+    bgColor: "rgba(3, 78, 253, 0.1)",
+    border: "rgba(3, 78, 253, 1)",
+    color: "rgba(3, 78, 253, 1)"
+  }
+};
+
 export default {
   salmon: "rgba(221, 115, 115, 1)",
   purple: "rgba(59, 53, 97, 1)",
