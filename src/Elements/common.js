50fff12d5974d2930a5abff51c6e3cb1cad599fb
Wesley Chang <weschang15@gmail.com>
2020-06-27 18:22:45 -0700

    feat: idk

---
+++ src/Elements/common.js
@@ -0,0 +1,26 @@
+export const ALIGN_ITEMS = [
+  "flex-start",
+  "flex-end",
+  "baseline",
+  "center",
+  "stretch"
+];
+
+export const JUSTIFY_CONTENT = [
+  ...ALIGN_ITEMS,
+  "start",
+  "end",
+  "space-between",
+  "space-around",
+  "space-evenly"
+];
+
+export const FLEX_DIRECTION = [
+  "row",
+  "row-reverse",
+  "column",
+  "column-reverse",
+  "row-wrap"
+];
+
+export const FLEX_WRAP = ["wrap", "nowrap"];
