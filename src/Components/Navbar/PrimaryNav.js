8eccea85fd0acad3f844f22c0ed624f604e35429
Wesley Chang <weschang15@gmail.com>
2020-06-27 18:24:07 -0700

    feat: idr

---
+++ src/Components/Navbar/PrimaryNav.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { useMutation } from "react-apollo";
+import { NavLink } from "react-router-dom";
+import { LOGOUT_USER } from "../../actions";
+import { AuthDispatch } from "../../contexts/AuthContext";
+import { Icons, Nav } from "../../Elements";
+import { LOGOUT } from "../../Mutations";
+
+function PrimaryNav() {
+  const dispatch = useContext(AuthDispatch);
+  const [logout] = useMutation(LOGOUT);
+
+  return (
+    <Nav.PrimaryNav>
+      <NavLink to="/" className="logo" exact>
+        <Icons icon="logo" fill="currentColor" />
+      </NavLink>
+      <Nav.NavList>
+        <Nav.NavListItem>
+          <NavLink to="/" exact>
+            <Icons fill="currentColor" />
+          </NavLink>
+        </Nav.NavListItem>
+        <Nav.NavListItem>
+          <NavLink to="/sites">
+            <Icons icon="window" fill="currentColor" />
+          </NavLink>
+        </Nav.NavListItem>
+        <Nav.NavListItem>
+          <span
+            onClick={async () => {
+              await logout();
+              dispatch({ type: LOGOUT_USER });
+            }}
+          >
+            <Icons icon="power" fill="currentColor" />
+          </span>
+        </Nav.NavListItem>
+      </Nav.NavList>
+    </Nav.PrimaryNav>
+  );
+}
+
+export default PrimaryNav;
