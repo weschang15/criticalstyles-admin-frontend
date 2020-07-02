09518b5362accfc29e9ab985da88ea30f9c4d5e8
Wesley Chang <weschang15@gmail.com>
2020-07-01 20:59:19 -0700

    feat: add onError prop for handling errors

--- src/Components/Tokens/Token.js
+++ src/Components/Tokens/Token.js
@@ -1,21 +1,33 @@
+import PropTypes from "prop-types";
 import React from "react";
 import { useMutation } from "react-apollo";
 import { LinkButton, Spinner, TableCell, TableRow } from "../../Elements";
 import { DELETE_TOKEN } from "../../Mutations";
 import ScaryButton from "../ScaryButton/ScaryButton";
 
-function Token({ createdAt, _id, secret }) {
-  const [deleteToken, { loading }] = useMutation(DELETE_TOKEN, {
-    variables: { input: { _id } },
-    refetchQueries: ["GetTokens"],
-  });
+function Token({ createdAt, _id, secret, onError }) {
+  const [deleteToken, { loading }] = useMutation(DELETE_TOKEN);
+
+  const handleDelete = async () => {
+    const { data } = await deleteToken({
+      variables: { input: { _id } },
+      refetchQueries: ["GetTokens"],
+    });
+
+    const ok = (data.deleteToken || {}).ok || false;
+    const errors = (data.deleteToken || {}).errors || [];
+
+    if (!ok) {
+      onError(errors);
+    }
+  };
 
   return (
     <TableRow>
       <TableCell>{secret}</TableCell>
       <TableCell>{new Date(createdAt).toLocaleString()}</TableCell>
       <TableCell>
-        <ScaryButton action={deleteToken}>
+        <ScaryButton action={handleDelete}>
           {({ message, onPress }) => (
             <LinkButton onClick={onPress}>
               {loading ? <Spinner bgColor="teal" /> : message}
@@ -26,5 +38,13 @@ function Token({ createdAt, _id, secret }) {
     </TableRow>
   );
 }
+
+Token.defaultProps = {
+  onError: () => null,
+};
+
+Token.propTypes = {
+  onError: PropTypes.func.isRequired,
+};
 
 export default Token;
--- src/Components/Tokens/Tokens.js
+++ src/Components/Tokens/Tokens.js
@@ -1,45 +1,48 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import { Table, TableBody, TableHeader, TableRow } from "../../Elements";
 import Skeleton from "../Skeleton/Skeleton";
+import Snackbar from "../Snackbar/Snackbar";
 import NoTokens from "./NoTokens";
 import Token from "./Token";
 
 function Tokens({ loading, tokens }) {
+  const [errors, setErrors] = useState(null);
+
   if (loading) {
-    return (
+    return <Skeleton labels={["name", "created at", "actions"]} />;
+  }
+
+  if (!tokens.length) {
+    return <NoTokens />;
+  }
+
+  return (
+    <>
+      {errors && (
+        <Snackbar type="error">
+          <ul>
+            {errors.map(({ message }, i) => (
+              <li key={i}>{message}</li>
+            ))}
+          </ul>
+        </Snackbar>
+      )}
       <Table>
         <TableHeader>
           <TableRow>
-            <th>name</th>
+            <th>secret</th>
             <th>created at</th>
             <th>actions</th>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <Skeleton />
+          {tokens.map((token) => (
+            <Token key={token._id} {...token} onError={setErrors} />
+          ))}
         </TableBody>
       </Table>
-    );
-  }
-
-  return tokens.length ? (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <th>secret</th>
-          <th>created at</th>
-          <th>actions</th>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {tokens.map((token) => (
-          <Token key={token._id} {...token} />
-        ))}
-      </TableBody>
-    </Table>
-  ) : (
-    <NoTokens />
+    </>
   );
 }
 
