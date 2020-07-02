ec0bde0305ac6324816d555f08fa138151a1b8c9
Wesley Chang <weschang15@gmail.com>
2020-07-01 20:59:41 -0700

    refactor: render entire table

--- src/Components/Skeleton/Skeleton.js
+++ src/Components/Skeleton/Skeleton.js
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import React, { useMemo } from "react";
+import { Table, TableBody, TableHeader, TableRow } from "../../Elements";
 import SkeletonItem from "./SkeletonItem";
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function Skeleton({ rowCount, colCount, className }) {
-  const items = useMemo(() => {
+function Skeleton({ rowCount, colCount, className, labels }) {
+  const rows = useMemo(() => {
     return Array.from({ length: rowCount }, (_, i) => {
       const size = getRndInteger(40, 75);
       return (
@@ -21,18 +22,28 @@ function Skeleton({ rowCount, colCount, className }) {
     });
   }, [rowCount, colCount, className]);
 
-  return <>{items}</>;
+  const cells = labels.map((label, i) => <th key={i.toString()}>{label}</th>);
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>{cells}</TableRow>
+      </TableHeader>
+      <TableBody>{rows}</TableBody>
+    </Table>
+  );
 }
 
 Skeleton.propTypes = {
+  labels: PropTypes.arrayOf(PropTypes.string).isRequired,
   rowCount: PropTypes.number.isRequired,
-  colCount: PropTypes.number
+  colCount: PropTypes.number,
 };
 
 Skeleton.defaultProps = {
   rowCount: 4,
   colCount: 3,
-  className: "no-hover"
+  className: "no-hover",
 };
 
 export default Skeleton;
